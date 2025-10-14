a86c28d195c61c4ca9830ee976dfb28083a812dc
Daniel <osva604@gmail.com>
2025-10-14 15:53:05 -0300

    v1.2

--- inicio.js
+++ inicio.js
@@ -1,6 +1,6 @@
 function validar() {
-  const password = 123456;
-  const usuario = "dmoya";
+  const password = "admin";
+  const usuario = "admin";
 
   const inputUsuario = document.getElementById("username").value;
   const inputPassword = document.getElementById("password").value;
